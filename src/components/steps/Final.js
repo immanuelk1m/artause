ae6411194498a8d655666e38dc07a7539354d6c4
immanuelk1m <kse0119@naver.com>
2024-01-04 15:59:54 +0900

    update Result.js

--- src/components/steps/Final.js
+++ src/components/steps/Final.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'; // Import Link
+
 export default function Final() {
   return (
     <div className="container md:mt-10">
@@ -10,11 +12,11 @@ export default function Final() {
         <div className="text-lg font-semibold text-gray-500">
           사는 곳과 취향 A.I. 분석하고 공연을 추천 받자!
         </div>
-        <a className="mt-10" href="/result">
+        <Link className="mt-10" to="/result">
           <button className="h-10 px-5 text-green-700 transition-colors duration-150 border border-gray-300 rounded-lg focus:shadow-outline hover:bg-green-500 hover:text-green-100">
             추천 공연 확인하기
           </button>
-        </a>
+        </Link>
       </div>
     </div>
   );
