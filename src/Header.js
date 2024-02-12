16792519d5294e48e47a89462bc1bf11069ee6d3
immanuelk1m <98140860+immanuelk1m@users.noreply.github.com>
2024-02-12 15:32:53 +0900

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -8,14 +8,10 @@ function Header() {
         <header className="brand-color text-white py-4">
             <div className="container d-flex justify-content-between align-items-center">
                 <div className="d-flex align-items-center">
-<<<<<<< HEAD
                     <a className="font-weight-bold h2 mb-0" href="https://www.artause.co.kr/">Artause</a>
-=======
-                    {/* 이미지를 a 태그로 감싸서 링크를 추가합니다 */}
                     <a href="https://www.artause.co.kr/" style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }}>
                         <img src={logoImage} alt="Artuase Logo" width="180" height="100" />
                     </a>
->>>>>>> b09281d1c7cebe3ef0a657e9ddbffe818dfccee1
                 </div>
                 <div className="menu-items">
                     <a href="https://blog.naver.com/artause" className="text-white px-2">Blog</a>
